150175db570b66ed7294c86fdcfdbdb38abf0382
Zulfafik <71050451+Zulfafik@users.noreply.github.com>
2022-01-11 11:47:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// JsQuery
+$(document).ready(function () {
+  // Slide kekanan
+  $('.next').on('click', function () {
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
+
+    if (nextImg.length) {
+      currentImg.removeClass('active').css('z-index', -10);
+      nextImg.addClass('active').css('z-index', 10);
+    }
+  });
+
+  // Slide kekiri
+  $('.prev').on('click', function () {
+    var currentImg = $('.active');
+    var prevImg = currentImg.prev();
+
+    if (prevImg.length) {
+      currentImg.removeClass('active').css('z-index', -10);
+      prevImg.addClass('active').css('z-index', 10);
+    }
+  });
+});
